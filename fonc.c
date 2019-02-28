6e8eabdb522ce615826de347977afce430ff1982
Obanda Vivien-Junior <s174043@ic2s120-09.univ-lemans.fr>
2019-02-28 10:52:49 +0100

    first commit

---
+++ fonc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 11
+
+char map[N][N];
+
+void affichage_sort(){
+	
+
+void affichage_map(char map[N][N]){
+
+	int i, j;
+
+	printf("Plan de jeu \n\n");
+
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++)
+			printf(" %c |", map[i][j]);
+		printf("\n");
+		
+		
+	}
+}
+
+
+int main(){
+
+	int i, j;
+
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++)
+			map[i][j] = 'a' + i + j;
+	}
+
+	affichage(map);
+}
+
