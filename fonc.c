bf6570032497f4196f5934fba73a79bfdfebc06f
Obanda Vivien-Junior <s174043@ic2s120-09.univ-lemans.fr>
2019-02-28 12:56:25 +0100

    affichage en c

--- fonc.c
+++ fonc.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fonc.h" 
 
-#define N 11
 
 char map[N][N];
 
-void affichage_sort(){
-	
+void affichage_sort(t_personnage perso){
+
+	printf("Personnage : %s\n\n", perso.nom);
+	printf("Listes des sorts: \n");
+
+	printf("- %s : ", perso.s1.nom);
+	printf("portée [%i] / ", perso.s1.portee);
+	printf("dégât [%i] / ", perso.s1.degat);
+	printf("point d'action [%i]\n", perso.s1.point_action);
+
+	printf("- %s : ", perso.s2.nom);
+	printf("portée [%i] / ", perso.s2.portee);
+	printf("dégât [%i] / ", perso.s2.degat);
+	printf("point d'action [%i]\n", perso.s2.point_action);
+
+	printf("- %s : ", perso.s3.nom);
+	printf("portée [%i] / ", perso.s3.portee);
+	printf("dégât [%i] / ", perso.s3.degat);
+	printf("point d'action [%i]\n", perso.s3.point_action);
+
+	printf("- %s : ", perso.s4.nom);
+	printf("portée [%i] / ", perso.s4.portee);
+	printf("dégât [%i] / ", perso.s4.degat);
+	printf("point d'action [%i]\n\n", perso.s4.point_action);
+
+}
 
 void affichage_map(char map[N][N]){
 
@@ -17,22 +41,29 @@ void affichage_map(char map[N][N]){
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++)
 			printf(" %c |", map[i][j]);
-		printf("\n");
-		
-		
+		printf("\n");			
 	}
 }
 
-
 int main(){
 
-	int i, j;
+	int i, j, classe;
 
+	t_personnage personnage;
+	printf("choisir classe\n");
+	scanf("%d",&classe);
+
+	personnage=creer_perso(classe,personnage);
+
+	/*à remplacer par fonction initialiser*/
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++)
 			map[i][j] = 'a' + i + j;
 	}
+	/*************************************/
 
-	affichage(map);
+	affichage_sort(personnage);
+	affichage_map(map);
+	return 0;
 }
 
