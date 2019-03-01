a6113685eeaf80407ca24b22e13f7059934e7860
Renaudin Dylan <DylanRenaudin@github.com>
2019-03-01 11:31:29 +0100

    deplacement des persos

---
+++ deplacement.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#include <fonc.h>
+
+#define N 11
+
+/*Fonction permettant de deplacer son personnage et changer ses coordonnées*/
+
+int deplacement(t_coordonnees coord){
+
+	int x,y;
+
+	do{
+		printf("saisir les coordonnées de la case ou vous voulez aller\n");
+
+		scanf("%d %d",&x,&y);
+	}
+	while(((fabs(coord.x-x)+fabs(coord.y-y))<=3) && (x<0 || x>=N) && (y<0 || y>=N));
+
+	coord.x = x;
+
+	coord.y = y;
+
+	return(coord);
+}
+
+int main(){
+
+
+
+	return 0;
+}
