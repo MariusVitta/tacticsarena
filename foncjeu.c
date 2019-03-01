6a2efd7fdc32b6bea00c3b41b36847143774e97b
Vivien-Junior <48085406+Vivien-Junior@users.noreply.github.com>
2019-03-01 12:00:41 +0100

    Fonction pour le déroulement de jeu

--- foncjeu.c
+++ foncjeu.c
@@ -6,3 +6,13 @@ int est_mort(t_personnage perso){
 	return (perso.pv <= 0);
 }
 
+void maj(char map[N][N], t_personnage perso){
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			if(j = perso.coord.x && i = perso.coord.y)
+				map[i][j] = '*';
+			else
+				map[i][j] = '.';
+		}
+	} 			
+}
