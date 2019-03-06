c67bdee33d4a383d36d04eef11fb2ac2743d7aeb
Vivien-Junior <48085406+Vivien-Junior@users.noreply.github.com>
2019-03-06 14:16:51 +0100

    Maj

--- foncjeu.c
+++ foncjeu.c
@@ -6,15 +6,11 @@ int est_mort(t_personnage perso){
 	return (perso.pv <= 0);
 }
 
-void maj(char map[N][N], t_personnage perso){
-<<<<<<< HEAD
-	int i,j;
-=======
+void maj(char map[N][N], t_personnage * perso){
 	int i, j;
->>>>>>> dc67b1808d6f35874fb6c8d4afbcd1ce32185a95
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++){
-			if(j == perso.coord.x && i == perso.coord.y)
+			if(j == perso->coord.x && i == perso->coord.y)
 				map[i][j] = '*';
 			else
 				map[i][j] = '.';
