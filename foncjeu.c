68141667253392aa9cf4ac505532b6daba1e08c6
Vivien-Junior <48085406+Vivien-Junior@users.noreply.github.com>
2019-03-06 14:37:36 +0100

    Mise à jour

--- foncjeu.c
+++ foncjeu.c
@@ -6,12 +6,14 @@ int est_mort(t_personnage perso){
 	return (perso.pv <= 0);
 }
 
-void maj(char map[N][N], t_personnage * perso){
+void maj(char map[N][N], t_personnage perso1, t_personnage perso2){
 	int i, j;
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++){
-			if(j == perso->coord.x && i == perso->coord.y)
+			if(j == perso1.coord.x && i == perso1.coord.y)
 				map[i][j] = '*';
+			if(j == perso2.coord.x && i == perso2.coord.y)
+				map[i][j] = '$';
 			else
 				map[i][j] = '.';
 		}
