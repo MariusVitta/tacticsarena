685991bd0bc92aca361721a0bac2187c029a30a2
MariusVitta <MariusVitta@github.com>
2019-03-06 13:54:41 +0100

    init

--- foncjeu.c
+++ foncjeu.c
@@ -7,6 +7,7 @@ int est_mort(t_personnage perso){
 }
 
 void maj(char map[N][N], t_personnage perso){
+	int i,j;
 	for(i = 0; i < N; i++){
 		for(j = 0; j < N; j++){
 			if(j == perso.coord.x && i == perso.coord.y)
