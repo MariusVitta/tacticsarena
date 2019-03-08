ad00f90f26edfee5613805901ea1efb317c51f40
MariusVitta <MariusVitta@github.com>
2019-03-08 15:11:42 +0100

    ajout de commentaire

--- fonc.h
+++ fonc.h
@@ -15,7 +15,7 @@ typedef struct s_sort{
 /*structure contenant les coordonnées du personnage*/
 
 typedef struct s_coordonnees{
-	
+
 	int x;
 	int y;
 
@@ -33,18 +33,23 @@ typedef struct s_personnage{
 	t_sort s1,s2,s3,s4;
 
 }t_personnage;
-
+/*fonction qui crée un personnage(affection des sorts,vie,points d'actions pour le type de personnage choisi) */
 t_personnage creer_perso(int,t_personnage);
+/*affichage de tout les sorts du personnage choisi pour le joueur*/
 void affichage_sort(t_personnage);
+/* affichage de la matrice */
 void affichage_map(char map[N][N]);
+/* fonction booleenne qui renvoie vrai si la vie du personnage est inférieur ou égale à zéro */
 int est_mort(t_personnage);
+/* mise à jour de la carte suite à une déplacement d'un joueur */
 void maj(char map[N][N], t_personnage perso1, t_personnage perso2);
+/* fonction déplacement case par case pour le joueur */
 int deplacement(t_personnage * j1,t_personnage j2);
+/*fonction d'initialisation (positionnement,création des obstacles) */
 void initialisation(char matriceJeu[N][N],t_personnage * perso1,t_personnage * perso2);
+/* affichage des coordonnées actuelles du personnage */
 void affichage_coord(t_personnage perso);
+/* affichage des sorts utilisable par le joueur */
 void sort_uti(t_personnage perso);
-
-
-
-
-
+/*fonction de tour de jeu (se déplacer,utiliser un sort, passer son tour) */
+int tour(char map[N][N],t_personnage * j1,t_personnage * j2 );
