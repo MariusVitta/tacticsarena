f3043642f87aa6024590667187302850bfa94b5c
MariusVitta <MariusVitta@github.com>
2019-03-12 16:03:42 +0100

    main avancé + tour jeu fini

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ char map[N][N];
 
 int main(){
 
-	int i, j, classe1, classe2,nb_tour = 1;
+	int i, j, classe1, classe2,nb_tour = 1,mort1 = 0,mort2 = 0;
     int num_j = 1;
 	t_personnage personnage1,personnage2;
 
@@ -37,26 +37,32 @@ int main(){
 
 	personnage1 = creer_perso(classe1,&personnage1);
 	personnage2 = creer_perso(classe2,&personnage2);
+    personnage1.pv = 10;
+    personnage2.pv = 5;
 
 	initialisation(map,&personnage1,&personnage2);
     printf("===================================================\n\tDEMARRAGE DE LA  PARTIE\n===================================================\n");
     affichage_map(map);
 
     /* boucle principale du jeu */
-    while(!est_mort(personnage1) || !est_mort(personnage2)){
+    while(!est_mort(personnage1) && !est_mort(personnage2)){
         printf("[Tour numéro:%i][Tour du joueur %i]\n\n",nb_tour,num_j);
-
         tour(map,&personnage1,&personnage2,1);
         affichage_map(map);
         num_j++;
-
-        printf("[Tour numéro:%i][Tour du joueur %i]\n\n",nb_tour,num_j);
-
-        tour(map,&personnage2,&personnage1,2);
-        affichage_map(map);
-		printf("===================================================\n\tAFFICHAGE COORDONNEES\n===================================================\n");
-		affichage_coord(personnage2);
-		affichage_coord(personnage1);
+        mort1 = est_mort(personnage1);
+        /* si le personnage 1 est mort on effectue pas le tour du joueur */
+        if(!mort1){
+            printf("[Tour numéro:%i][Tour du joueur %i]\n\n",nb_tour,num_j);
+            tour(map,&personnage2,&personnage1,2);
+            affichage_map(map);
+        }
+        mort2= est_mort(personnage2);
+        if( mort1 || mort2 ){
+		          printf("===================================================\n\tAFFICHAGE COORDONNEES\n===================================================\n");
+		          affichage_coord(personnage2);
+		          affichage_coord(personnage1);
+        }
         num_j--;
         nb_tour++;
    }
