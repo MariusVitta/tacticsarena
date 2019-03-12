7087faaa035a49daf9fe56762fba37fd718e938f
MariusVitta <MariusVitta@github.com>
2019-03-12 10:32:23 +0100

    tour jeu

--- tour_jeu.c
+++ tour_jeu.c
@@ -10,7 +10,7 @@
  * la fonction demande au personne s'il souhaite effectuer un déplacement/utiliser un ou des sort(s)/passer son tour et ne rien faire
  * renvoie vrai lors que le tour de jeu du personnage est finie ou lorsqu'il passe son tour
  */
-int tour(char map[N][N],t_personnage j1,t_personnage j2,int nb_j ){
+int tour(char map[N][N],t_personnage * j1,t_personnage * j2,int nb_j ){
     /* variable qui compte le nombre de déplacement max possible par personnage*/
     int nb_deplacement = 3;
     int choix = 0;
@@ -25,7 +25,7 @@ int tour(char map[N][N],t_personnage j1,t_personnage j2,int nb_j ){
         switch(choix){
             case 1:
                 if(nb_deplacement > 0 ){
-                    nb_deplacement = deplacement(&j1,j2,map,nb_deplacement,nb_j);break;
+                    nb_deplacement = deplacement(j1,j2,map,nb_deplacement,nb_j);break;
                 }
                 else{
                     printf("\n ---- Vous avez utilisé tous vos points de déplacements ----\n\n");break;
@@ -40,17 +40,17 @@ int tour(char map[N][N],t_personnage j1,t_personnage j2,int nb_j ){
                     while(choix < 1 || choix > 4);
                     if(j1.nom == "Guerrier"){
                         switch(choix){
-                            case 1:saut(&j1,map);break;
+                            case 1:saut(j1,map);break;
                             case 2:/*perso.s2=soin;*/break;
-                            case 3: petit_coup(map,j1,&j2);break;
+                            case 3: petit_coup(map,j1,j2);break;
                             case 4:/*perso.s4=groscoup;*/break;
                         }
                     }
                     else if(j1.nom == "Archer"){
                         switch(choix){
-                            case 1:diago(map,j1,&j2);break;
+                            case 1:diago(map,j1,j2);break;
                             case 2:/*perso.s2=ligne;*/break;
-                            case 3:double_tape(map,j1,&j2);break;
+                            case 3:double_tape(map,j1,j2);break;
                             case 4:/*perso.s4=coupzone;*/break;
                         }
                     }
