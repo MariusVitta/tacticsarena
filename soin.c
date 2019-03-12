dacaddbac6c663ab027fbe22743ed2b0c59faadf
Renaudin Dylan <DylanRenaudin@github.com>
2019-03-12 14:43:45 +0100

    soin correction

--- soin.c
+++ soin.c
@@ -5,10 +5,10 @@
 
 void soin(t_personnage * j1) {
   printf("Le guerrier a %d points de vie\n",j1->pv);
-  if(j1->pv+j1->s2.degat<=60)
+  if(j1->pv+j1->s2.degat<=j1->pv_max)
     j1->pv+=j1->s2.degat;
   else
-    j1->pv=60;
+    j1->pv=j1->pv_max;
 
-  printf("Le guerrier se soigne de %d et a maintenant %d points de vie\n",j1->s2.degat,j1->pv);
+  printf("Le guerrier se soigne de %d et a maintenant %d points de vie sur %d\n",j1->s2.degat,j1->pv,j1->pv_max);
 }
