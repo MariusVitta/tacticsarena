3a8023830ff03b42a38d135e248f74c84c1da49d
Vivien-Junior <48085406+Vivien-Junior@users.noreply.github.com>
2019-04-05 16:13:14 +0200

    SDL fichiers

--- sdl_fonc.h
+++ sdl_fonc.h
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <math.h>
 #include <SDL2/SDL.h>
-#include<SDL2/SDL_ttf.h>
-#include<SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
 #ifndef LARGEUR
-#define LARGEUR 1300
+#define LARGEUR 1340
 #endif
 #ifndef HAUTEUR
-#define HAUTEUR 850
+#define HAUTEUR 800
 #endif
 #ifndef N
 #define N 11
@@ -21,18 +22,44 @@
 
 extern SDL_Window* window;
 extern SDL_Renderer *renderer;
+extern SDL_Texture * texte;
 
 extern SDL_Texture * bg;
+extern SDL_Texture * accueil;
+extern SDL_Texture * accueil1;
 extern SDL_Texture * case_n;
 extern SDL_Texture * case_p;
 extern SDL_Texture * case_c;
 extern SDL_Texture * case_s;
 extern SDL_Texture * obstacle;
-extern SDL_Texture * guerrier;
-extern SDL_Texture * archer;
-extern SDL_Texture * tank;
+extern SDL_Texture * guerrier1;
+extern SDL_Texture * archer1;
+extern SDL_Texture * tank1;
+extern SDL_Texture * druide1;
+
+extern SDL_Texture * guerrier2;
+extern SDL_Texture * archer2;
+extern SDL_Texture * tank2;
+extern SDL_Texture * druide2;
+
+extern SDL_Texture * idguerrier1;
+extern SDL_Texture * idarcher1;
+extern SDL_Texture * idtank1;
+extern SDL_Texture * iddruide1;
+
+extern SDL_Texture * idguerrier2;
+extern SDL_Texture * idarcher2;
+extern SDL_Texture * idtank2;
+extern SDL_Texture * iddruide2;
 
 extern SDL_Texture * choix_perso;
+
+extern TTF_Font * police;
+
+extern SDL_Color noir;
+extern SDL_Color blanc;
+extern SDL_Color orange;
+extern SDL_Color rouge;
 
 typedef enum {CASE_GRISE, CASE_BLEUE, OBSTACLE, J1P1, J1P2, J2P1, J2P2}t_type_case;
 
@@ -62,14 +89,21 @@ t_case SDL_map[N * N];
 t_choix SDL_choix_p[NBPERSOS];
 
 void SDL_choix_perso();
+void SDL_afficher_bg();
+void SDL_afficher_acceuil();
 void SDL_afficher_map(char map[N][N], t_joueur j1, t_joueur j2);
+void SDL_afficher_message(char * message, int x, int y, char * police, int taille, char couleur, char style);
 int SDL_creer_fenetre();
 void SDL_creer_texture();
+void SDL_afficher_sort(char * nom, int xsort, int ysort);
+SDL_Texture * SDL_charger_image (char * image);
+void SDL_ecrire_texte(char * chaine, char * police_ec, int taille, char color, char style);
 float eq_droite(t_coord p1, t_coord p2, int x);
 int dans_case(t_coord a, t_coord b, t_coord c, t_coord d, int x, int y);
 int acces_possible(int x, int y, int * ymap, int * xmap);
 int acces_choix_perso(int x, int y, int * classe);
-void SDL_affichage_choix_perso();
+void SDL_affichage_choix_perso(int num_j);
+void SDL_afficher_perso_choisi(t_joueur joueur1, t_joueur joueur2);
 void SDL_choix_perso(t_joueur *joueur1, t_joueur *joueur2);
 
 void SDL_initialisation(char matriceJeu[N][N],t_joueur * joueur1,t_joueur * joueur2);
--- sdl_main.c
+++ sdl_main.c
@@ -4,17 +4,47 @@
 SDL_Window* window = NULL;
 SDL_Renderer *renderer = NULL;
 
+SDL_Texture * texte = NULL;
 SDL_Texture * bg = NULL;
+SDL_Texture * accueil = NULL;
+SDL_Texture * accueil1 = NULL;
 SDL_Texture * case_n = NULL;
 SDL_Texture * case_p = NULL;
 SDL_Texture * case_c = NULL;
 SDL_Texture * case_s = NULL;
 SDL_Texture * obstacle = NULL;
-SDL_Texture * guerrier = NULL;
-SDL_Texture * archer = NULL;
-SDL_Texture * tank = NULL;
+/*Texture joueur 1*/
+SDL_Texture * guerrier1 = NULL;
+SDL_Texture * archer1 = NULL;
+SDL_Texture * tank1 = NULL;
+SDL_Texture * druide1 = NULL;
+/*Texture joueur 2*/
+SDL_Texture * guerrier2 = NULL;
+SDL_Texture * archer2 = NULL;
+SDL_Texture * tank2 = NULL;
+SDL_Texture * druide2 = NULL;
+
+/*Texture id joueur 1*/
+SDL_Texture * idguerrier1 = NULL;
+SDL_Texture * idarcher1 = NULL;
+SDL_Texture * idtank1 = NULL;
+SDL_Texture * iddruide1 = NULL;
+/*Texture id joueur 2*/
+SDL_Texture * idguerrier2 = NULL;
+SDL_Texture * idarcher2 = NULL;
+SDL_Texture * idtank2 = NULL;
+SDL_Texture * iddruide2 = NULL;
 
 SDL_Texture * choix_perso = NULL;
+
+TTF_Font * police = NULL;
+
+/*Liste de couleur*/
+
+SDL_Color noir = {0, 0, 0};
+SDL_Color blanc = {255, 255, 255};
+SDL_Color orange = {204, 102, 0};
+SDL_Color rouge = {139, 0, 0};
 
 int main (int argc, char** argv){
 
@@ -47,12 +77,14 @@ int main (int argc, char** argv){
 		fprintf(stderr, "Erreur à la création de la fenetre : %s\n", SDL_GetError());
 	SDL_creer_texture();
 
+	SDL_afficher_acceuil();
+
 	//SDL_affichage_choix_perso();
 	//SDL_Delay(5000);
 	SDL_choix_perso(&joueur1, &joueur2);
 	SDL_initialisation(map,&joueur1,&joueur2);
 	//initialisation(map,&joueur1,&joueur2);
-	int nb = 3;
+	/*int nb = 3;
 	if(nb > 0)
 		nb = SDL_deplacement(joueur1, joueur2, map, nb, 1, 1);
 	if(nb > 0)
@@ -61,7 +93,12 @@ int main (int argc, char** argv){
 		nb = SDL_deplacement(joueur1, joueur2, map, nb, 1, 1);
 	//SDL_saut(joueur1, joueur2, map, 1, 1);*/
 	int x = 0, y = 0;
-	//SDL_afficher_map(map, joueur1, joueur2);
+	SDL_afficher_message("Go!", 519, 306, "Police/RunDemo.ttf", 110, 'r', 'N');
+	SDL_RenderPresent(renderer);
+	SDL_Delay(1000);
+
+	//SDL_affichage_choix_perso(1);
+
 
 	if( window )
 	{
@@ -96,9 +133,8 @@ int main (int argc, char** argv){
 			}
 			SDL_RenderClear(renderer);
 			SDL_afficher_map(map, joueur1, joueur2);
-			//SDL_affichage_choix_perso();
+			//SDL_affichage_choix_perso(2);
 			SDL_RenderPresent(renderer);
-			SDL_Delay(16);
 		}
 	} else
 		fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
@@ -106,9 +142,12 @@ int main (int argc, char** argv){
 	/*for(int i = 0; i < N*N; i++)
 		printf("case %i - %i : a(%i;%i)  b(%i;%i)  c(%i;%i) d(%i;%i)\n", SDL_map[i].i, SDL_map[i].j, SDL_map[i].a.x,SDL_map[i].a.y, SDL_map[i].b.x, SDL_map[i].b.y, SDL_map[i].c.x, SDL_map[i].c.y, SDL_map[i].d.x, SDL_map[i].d.y);
 	*/
-	SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
-    return EXIT_SUCCESS;
+	SDL_DestroyRenderer(renderer);
+  	SDL_DestroyWindow(window);
+	TTF_CloseFont(police); /* Doit être avant TTF_Quit() */
+	TTF_Quit();
+  	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
